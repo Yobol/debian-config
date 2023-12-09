1414e24c68d270660d3b78fec1c491bea3630ef1
yobol <yobol1024@gmail.com>
2023-12-09 08:40:00 +0000

    update vim config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+sudo apt-get update &&
+     apt-get install -y --no-install-recommends \
+     vim \
+     zsh
+
+KERNEL=`uname`
+if [ ${KERNEL} == 'Linux' ] ; then
+	LN=ln
+	MKDIR=mkdir
+	RMDIR='rm -r'
+        CPDIR='cp -r'
+	CP=cp
+	RM=rm
+else
+	echo "UNSUPPORTED KERNEL: ${KERNEL}"
+	exit 0
+fi
+
+PWD=`pwd`
+
 # vim
-cp --recursive vim ~/.vim
-mv ~/.vim/vimrc ~/.vimrc
+${RM} ~/.vimrc && ${LN} -sT ${PWD}/.vimrc ~/.vimrc
+${RMDIR} ~/.vim &&  ${LN} -sT ${PWD}/.vim ~/.vim
---
+++ vim/.vimrc
@@ -0,0 +1,9 @@
+" enable highlight
+syntax on
+
+" allow to use mouse, such as selecting multiple lines, copying and pasting
+set mouse=
+
+" show line numbers
+set number
+
